f5c38adf04c96080967903f867cd27a3baf7fbcc
VersusDebias <versusdebias@163.com>
2024-07-31 12:43:22 +0800

    Update origin_array.py

--- tools/origin_array.py
+++ tools/origin_array.py
@@ -16,22 +16,20 @@ def generate_repeated_keys(data):
     result = {}
     for key in data.keys():
         cleaned_key = clean_string(key)
-        result[cleaned_key] = [clean_string(key)] * 10
+        result[cleaned_key] = [clean_string(key)] * 20 # change your origin array num (default 20)
     return result
 
 def write_json_file(data, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-# 主函数
 def main(input_file, output_file):
     data = read_json_file(input_file)
     repeated_keys_data = generate_repeated_keys(data)
     write_json_file(repeated_keys_data, output_file)
     print(f"JSON with repeated keys has been saved to {output_file}")
 
-# 使用示例
-input_file = './debias/data/truth.json'  # 输入的JSON文件
-output_file = './debias/data/origin_array.json'  # 输出的JSON文件
+input_file = './debias/data/truth.json'
+output_file = './debias/data/origin_array.json'
 main(input_file, output_file)
 
