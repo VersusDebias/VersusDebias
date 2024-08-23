3f4f79ebbb5214cc462a6584b7477a1a8e6ec52d
VersusDebias <versusdebias@163.com>
2024-08-24 00:51:17 +0800

    Update test_finetune.py

--- debias/test_finetune.py
+++ debias/test_finetune.py
@@ -3,6 +3,6 @@ import torchvision
 import transformers
 print(torchvision.__version__)
 print(transformers.__version__)
-main_directory = "/rsch/hanjun/VersusDebias/GAM_result/image/lcm/epoch1"
-output_directory = "/rsch/hanjun/VersusDebias/output"
+main_directory = "../GAM_result/image/lcm/epoch1"
+output_directory = "../VersusDebias/output"
 process_all_subdirs(main_directory, output_directory)
